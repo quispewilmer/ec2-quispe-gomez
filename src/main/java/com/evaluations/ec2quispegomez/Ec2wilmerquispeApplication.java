de2c83f4128a0ed09f8af0d5f8e884ffd81b814a
quispewilmer <wilmerquispecode@gmail.com>
2022-10-28 00:42:00 +0000

    Add base project

---
+++ src/main/java/com/evaluations/ec2quispegomez/Ec2wilmerquispeApplication.java
@@ -0,0 +1,13 @@
+package com.evaluations.ec2quispegomez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2wilmerquispeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2wilmerquispeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluations/ec2quispegomez/Ec2wilmerquispeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluations.ec2quispegomez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2wilmerquispeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
